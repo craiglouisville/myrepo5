f5437c9a66257b65e05cc1c93f1a4e4f536e4264
craig goldstein <craiglouisville@gmail.com>
2024-06-13 19:27:42 -0400

    update 6-13

--- getQuote.js
+++ getQuote.js
@@ -18,7 +18,6 @@ var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbo
 //     }
 // });
 
-
 async function getQuote(){
     try {
         let response = await fetch(url);
@@ -35,3 +34,27 @@ async function getQuote(){
 }
 
 getQuote ()
+
+
+
+// var request = require('request');
+
+// // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+// var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=KO&interval=5min&apikey=RLQUZ3LH4BWSZTG4';
+
+// request.get({
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else if (res.statusCode !== 200) {
+//       console.log('Status:', res.statusCode);
+//     } else {
+//       // data is successfully parsed as a JSON object:
+//       console.log(data);
+//     }
+// });
+
+// getQuote ()
--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// const express = require('express');
+// const axios = require('axios');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(cors());
+
+// app.get('/quote', async (req, res) => {
+//     const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=KO&interval=5min&apikey=RLQUZ3LH4BWSZTG4';
+
+//     try {
+//         const response = await axios.get(url, {
+//             headers: { 'User-Agent': 'axios' }
+//         });
+//         res.json(response.data);
+//     } catch (error) {
+//         res.status(500).send('Error fetching data');
+//     }
+// });
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
+
+
+
+
+
+
+
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
@@ -12,7 +42,19 @@ app.get('/quote', async (req, res) => {
         const response = await axios.get(url, {
             headers: { 'User-Agent': 'axios' }
         });
-        res.json(response.data);
+
+        // Filter the data to include only the timestamp at 16:00:00
+        const timeSeries = response.data['Time Series (5min)'];
+        const filteredData = {};
+
+        for (const [timestamp, values] of Object.entries(timeSeries)) {
+            if (timestamp.endsWith('16:00:00')) {
+                filteredData[timestamp] = values;
+                break; // Stop after finding the first match
+            }
+        }
+
+        res.json(filteredData);
     } catch (error) {
         res.status(500).send('Error fetching data');
     }
